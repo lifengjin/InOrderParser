474b4c0473f49125cb29076affaf5652d8e67409
jin <jin.544@osu.edu>
2018-09-14 12:03:56 -0400

    make rb tree

---
+++ scripts/make_rb.py
@@ -0,0 +1,16 @@
+import sys
+from nltk.tree import Tree as Tree
+
+with open(sys.argv[1]) as ofh:
+    trees = []
+    for line in ofh:
+        words_and_tags = line.strip().split(' ')
+        cur_frag = None
+        whole_len = len(words_and_tags)
+        for word_index in range(0, whole_len, 2):
+            preterm = Tree(words_and_tags[word_index+1], [words_and_tags[word_index],])
+            if not cur_frag:
+                cur_frag = preterm
+            else:
+                cur_frag = Tree('S' , [cur_frag, preterm])
+        print(cur_frag.pformat(margin=10000))
